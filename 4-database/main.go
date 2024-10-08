e11b1f7ca535002fd7ff033a3b8a5581015011c1
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2024-10-08 22:46:14 +0200

    query the database

--- 4-database/main.go
+++ 4-database/main.go
@@ -11,6 +11,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID		int64
+	Title	string
+	Artist	string
+	Price	string
+}
+
 func main() {
 	cfg := mysql.Config{
 		User: os.Getenv("DBUSER"),
@@ -24,10 +31,35 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
+    
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
 
-    pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-    fmt.Println("Connected!")
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
 }
