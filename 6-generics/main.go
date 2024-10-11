dd88503666973995032b32809a7a933f88b82ec1
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2024-10-11 11:41:30 +0200

    non-generic sum functions

---
+++ 6-generics/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Initialize a map for the integer values
+    ints := map[string]int64{
+        "first":  34,
+        "second": 12,
+    }
+
+    // Initialize a map for the float values
+    floats := map[string]float64{
+        "first":  35.98,
+        "second": 26.99,
+    }
+
+    fmt.Printf("Non-Generic Sums: %v and %v\n",
+        SumInts(ints),
+        SumFloats(floats))
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, value := range(m) {
+		s += value
+	}
+	return s
+}
+
+func SumFloats(values map[string]float64) float64 {
+	var sum float64
+	for _, value := range(values) {
+		sum += value
+	}
+	return sum
+}
